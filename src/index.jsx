9e4674c3a1c11eafe2e519170ede6bf53e62e93b
amandalchiu <amanda.chiu98@gmail.com>
2017-10-28 18:14:54 -0400

    add line graph example

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Toolbar from './toolbar.jsx'
+import LineGraph from './chart.jsx'
 
 
 export class Index extends React.PureComponent {
   render() {
     return (<div>
-            <Toolbar/>
+            <LineGraph />
+
             <h1>hi!</h1>
             <h2>Greetings, from Real Python!</h2>
             </div>);
