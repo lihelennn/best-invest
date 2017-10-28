2c6671ec6ad665d117ca9e8b3555040645429e64
HELEN LI <helen82198@gmail.com>
2017-10-28 15:00:57 -0400

    starting index

---
+++ templates/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export class Index extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (<h2>Greetings, from Real Python!</h2>);
+  }
+}
+
+export default Index;
