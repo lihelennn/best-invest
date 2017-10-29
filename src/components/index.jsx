be2c1689034c996b9dc567717cecd3c71edf4d89
amandalchiu <amanda.chiu98@gmail.com>
2017-10-29 04:59:42 -0400

    separate right left div

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,7 @@
 import React, {PropTypes} from 'react';
 // import { createStructuredSelector } from 'reselect';
+import styled from 'styled-components';
+
 import { connect } from 'react-redux';
 import Toolbar from './toolbar.jsx'
 import LineGraph from './chart.jsx'
@@ -7,6 +9,18 @@ import SearchBar from './searchBar.jsx'
 import SearchButton from './searchButton.jsx';
 // import { makeSelectSymbol } from '../main/selectors';
 import {getInfo, getOpen, getClose} from '../../static/api';
+
+export const wholeContainer = styled.div`
+  margin: 0; padding: 0; height: 100%;
+  background-color: #2c3e50;
+`;
+export const LeftCompartment = styled.div`
+  position: absolute; left: 0; height: 100%; width: 50%;
+`;
+export const RightCompartment = styled.div`
+  position: absolute; right: 0; height: 100%; width: 50%;
+`;
+
 
 export class Index extends React.PureComponent {
   render() {
@@ -19,14 +33,19 @@ export class Index extends React.PureComponent {
       console.log(getOpen(dates[i], sym));
       data1.push(getOpen(dates[i], sym));
     }
-    return (<div>
+    return (
+      <wholeContainer>
+        <LeftCompartment>
             <SearchBar/>
+            <h1>this is a test</h1>
             {this.props.symbol}
-
             <LineGraph data1={data1} labels={dates}/>
-
+        </ LeftCompartment>
+        <RightCompartment>
             <h2>Greetings, from Real Python!</h2>
-            </div>);
+        </ RightCompartment>
+      </ wholeContainer>
+    );
   }
 }
 
